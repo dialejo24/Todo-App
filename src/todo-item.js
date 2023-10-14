a314c59a692148cfa686bc6fe7908f7f43f9a425
Dialejo24 <dialejosi530@gmail.com>
2023-10-13 23:11:47 -0500

    Add functions to create, modify and retrieve todo-item properties

---
+++ src/todo-item.js
@@ -0,0 +1,27 @@
+function createTodoItem(description) {
+
+    return {
+        description,
+        state : "active",
+        changeState,
+        getState,
+        getDescription,
+    };
+}
+
+function changeState() {
+    if (this.state == "active") {
+        this.state = "completed";
+
+    } else {
+        this.state = "active";
+    }
+}
+
+function getState() {
+    return this.state;
+}
+
+function getDescription() {
+    return this.description;
+}
